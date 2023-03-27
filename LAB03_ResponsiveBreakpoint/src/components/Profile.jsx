3d08991ebb803cdde9e977c1ec0f64f1c63577cd
JAKEYSLINKY <115193557+JAKEYSLINKY@users.noreply.github.com>
2023-03-27 14:46:18 +0700

    27MAR2023 DONE

---
+++ LAB03_ResponsiveBreakpoint/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile() {
+	return (
+		<div className="layout">
+			<img src="public/assets/IMG_0338.jpeg" alt="Profile" />
+			<h1 className="name">JAKE SUTRUTRI</h1>
+			<hr></hr>
+		</div>
+	)
+}
+
+export default Profile;
